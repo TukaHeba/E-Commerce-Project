6f1cbd5371b0c3f0b19a5199f771385c74a6a466
Tuka Heba <tuka.heba@gmail.com>
2024-11-28 08:00:32 +0300

    Add-classes/Requests/Add needed requests

---
+++ app/Http/Requests/Category/StoreCategoryRequest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Requests\Category;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Prepare the data for validation.
+     * This method is called before validation starts to clean or normalize inputs.
+     * 
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            //
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+
+     /**
+     * Define human-readable attribute names for validation errors.
+     * 
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Define custom error messages for validation failures.
+     * 
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'required' => 'The :attribute field is required.',
+            'max' => 'The :attribute may not be greater than :max characters.',
+            'min' => 'The :attribute must be at least :min characters.',
+            'unique' => 'The :attribute has already been taken.',
+            'in' => 'The selected :attribute is invalid.',
+            'date' => 'The :attribute must be a valid date.',
+            'exists' => 'The selected :attribute is invalid.',
+        ];
+    }
+
+    /**
+     * Handle validation errors and throw an exception.
+     *
+     * @param \Illuminate\Contracts\Validation\Validator $validator The validation instance.
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors()->all();
+        throw new HttpResponseException(
+            response()->json([
+                'status' => 'error',
+                'message' => 'A server error has occurred',
+                'errors' => $errors,
+            ], 403)
+        );
+    }
+}
