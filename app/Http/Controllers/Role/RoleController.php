ea24decf22b0beab30ce6614ea2fcb6a63e52266
osama806 <osama20033210@gmail.com>
2024-12-03 23:42:20 +0300

    Edit-Feature/Role:controller, service/Move operations from controller to service

--- app/Http/Controllers/Role/RoleController.php
+++ app/Http/Controllers/Role/RoleController.php
@@ -6,34 +6,39 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Role\StoreRoleRequest;
 use App\Http\Requests\Role\UpdateRoleRequest;
 use App\Http\Resources\RoleResource;
+use App\Services\Role\RoleService;
 use Illuminate\Support\Facades\Cache;
 use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
+    protected $roleService;
+    public function __construct(RoleService $roleService)
+    {
+        $this->roleService = $roleService;
+    }
+
     /**
      * Get list of roles
-     * @return mixed|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        $roles = Cache::remember("roles", 600, function () {
-            return Role::all();
-        });
-
-        return self::success(RoleResource::collection($roles));
+        $response = $this->roleService->getAll();
+        return self::success(RoleResource::collection($response['roles']));
     }
 
     /**
      * Create new role in storage
      * @param \App\Http\Requests\Role\StoreRoleRequest $request
-     * @return mixed|\Illuminate\Http\JsonResponse
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(StoreRoleRequest $request)
     {
-        $role = Role::create($request->validated());
-
-        return self::success($role, 'Role created successfully', 201);
+        $response = $this->roleService->createNew($request->validated());
+        return $response['status']
+            ? self::success($response['role'], 'Role created successfully', 201)
+            : self::error(null, $response['msg'], $response['code']);
     }
 
     /**
@@ -58,22 +63,10 @@ class RoleController extends Controller
      */
     public function update(UpdateRoleRequest $request, $id)
     {
-        $role = Role::find($id);
-        if (!$role) {
-            return self::error('', 'Role not found', 404);
-        }
-
-        $filteredData = array_filter($request->validated(), function ($value) {
-            return !is_null($value) && trim($value) !== '';
-        });
-
-        if (count($filteredData) < 1) {
-            return self::error($filteredData, 'Not Found Any Data to Update', 404);
-        }
-
-        $role->update($filteredData);
-
-        return self::success(new RoleResource($role), 'Role updated successfully');
+        $response = $this->roleService->change($request->validated(), $id);
+        return $response['status']
+            ? self::success(new RoleResource($response['role']), 'Role Updated Successfully', 200)
+            : self::error(new RoleResource($response['role']), $response['msg'], $response['code']);
     }
 
     /**
---
+++ app/Services/Role/RoleService.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Services\Role;
+
+use Exception;
+use Illuminate\Support\Facades\Cache;
+use Spatie\Permission\Models\Role;
+
+class RoleService
+{
+    /**
+     * Get list of roles
+     * @return array
+     */
+    public function getAll()
+    {
+        $roles = Cache::remember("roles", 600, function () {
+            return Role::all();
+        });
+
+
+        return [
+            'status' => true,
+            'roles' => $roles
+        ];
+    }
+
+    /**
+     * Store role in storage
+     * @param array $data
+     * @return array
+     */
+    public function createNew(array $data)
+    {
+        try {
+            $role = Role::create($data);
+            return [
+                'status' => true,
+                'role' => $role
+            ];
+        } catch (Exception $e) {
+            return [
+                'status' => false,
+                'msg' => $e->getMessage(),
+                'code' => 500
+            ];
+        }
+
+    }
+
+    /**
+     * Update role info
+     * @param array $data
+     * @param string $id
+     * @return array
+     */
+    public function change(array $data, string $id)
+    {
+        $role = Role::find($id);
+        if (!$role) {
+            return [
+                'status' => false,
+                'role' => $role,
+                'msg' => 'Role not found',
+                'code' => 404
+            ];
+        }
+
+        $filteredData = array_filter($data, function ($value) {
+            return !is_null($value) && trim($value) !== '';
+        });
+
+        if (count($filteredData) < 1) {
+            return [
+                'status' => false,
+                'role' => $role,
+                'msg' => 'Not Found Any Data to Update',
+                'code' => 400
+            ];
+        }
+
+        $role->update($filteredData);
+        return ['status' => true];
+    }
+}
