a039e2d298c61bea9b033dec8b6a5a59e80379b5
Mona <moon.2060@gmail.com>
2024-12-23 18:34:40 +0300

    'fix-service | Main Category and SubCategory Service | remove try and catch'

--- app/Services/Category/MainCategoryService.php
+++ app/Services/Category/MainCategoryService.php
@@ -1,60 +1,61 @@
 <?php
+
 namespace App\Services\Category;
 
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Models\Category\MainCategory;
 
-class MainCategoryService{
+class MainCategoryService
+{
     /**
      * method to view all main categories with a filtes on (????)
      * @return /Illuminate\Http\JsonResponse if have an error
      */
-    public function getMainCategorys($request){
-        try {
-            $maincategories = MainCategory::with('subCategories')->get();
-            return $maincategories;
-        } catch (\Throwable $th) { Log::error($th->getMessage()); return Controller::error(null ,'Something went wrong with fetche main categories');}
+    public function getMainCategorys($request)
+    {
+
+        $maincategories = MainCategory::with('subCategories')->get();
+        return $maincategories;
     }
-//========================================================================================================================
+
     /**
      * method to creta new main category
      * @param   $data
      * @return /Illuminate\Http\JsonResponse if have an error
      */
-    public function storeMainCategory($data){
-        try {
-            $maincategory = new MainCategory();
-            $maincategory->main_category_name = $data['main_category_name'];
-            $maincategory->save();
+    public function storeMainCategory($data)
+    {
 
-            $maincategory->subCategories()->attach($data['sub_category_name']);
-            $maincategory->save();
+        $maincategory = new MainCategory();
+        $maincategory->main_category_name = $data['main_category_name'];
+        $maincategory->save();
 
-            return $maincategory;
-        } catch (\Throwable $th) { Log::error($th->getMessage()); return Controller::error(null ,'Something went wrong with create new main category');}
+        $maincategory->subCategories()->attach($data['sub_category_name']);
+        $maincategory->save();
+
+        return $maincategory;
     }
-//========================================================================================================================
+
     /**
      * method to update main category alraedy exist
      * @param   $data
      * @param   MainCategory $maincategory
      * @return /Illuminate\Http\JsonResponse if have an error
      */
-    public function updateMainCategory($data,MainCategory $maincategory){
-        try {
-            $maincategory->main_category_name = $data['main_category_name'] ?? $maincategory->main_category_name;
+    public function updateMainCategory($data, MainCategory $maincategory)
+    {
+        $maincategory->main_category_name = $data['main_category_name'] ?? $maincategory->main_category_name;
+        $maincategory->save();
+
+        if ($data['sub_category_name'] != null) {
+            $maincategory->subCategories()->sync($data['sub_category_name']);
             $maincategory->save();
+        }
 
-            if($data['sub_category_name'] != null){
-                $maincategory->subCategories()->sync($data['sub_category_name']);
-                $maincategory->save();
-            }
-
-            return $maincategory;
-        } catch (\Throwable $th) { Log::error($th->getMessage()); return Controller::error(null ,'Something went wrong with update new main category');}
+        return $maincategory;
     }
-//========================================================================================================================  
+
     /**
      * method to soft delete main category alraedy exist
      * @param  $id
@@ -62,14 +63,11 @@ class MainCategoryService{
      */
     public function destroyMainCategory($id)
     {
-        try {  
-            $mainCategory = MainCategory::findOrFail($id);
-            $mainCategory->delete();
-            $mainCategory->subCategories()->updateExistingPivot($mainCategory->subCategories->pluck('id'), ['deleted_at' => now()]); 
-            return true;
-        } catch (\Throwable $th) { Log::error($th->getMessage()); return Controller::error(null ,'Something went wrong with soft delete main category');}
+        $mainCategory = MainCategory::findOrFail($id);
+        $mainCategory->delete();
+        $mainCategory->subCategories()->updateExistingPivot($mainCategory->subCategories->pluck('id'), ['deleted_at' => now()]);
+        return true;
     }
-//========================================================================================================================
     /**
      * method to soft delete main category alraedy exist
      * @param  $id
@@ -77,12 +75,9 @@ class MainCategoryService{
      */
     public function restorMainCategory($id)
     {
-        try {  
-            $mainCategory = MainCategory::onlyTrashed()->findOrFail($id);
-            $mainCategory->subCategories()->withTrashed()->updateExistingPivot($mainCategory->subCategories->pluck('id'), ['deleted_at' => null]);
-            $mainCategory->restore();
-            return true;
-        } catch (\Throwable $th) { Log::error($th->getMessage()); return Controller::error(null ,'Something went wrong with restor main category');}
+        $mainCategory = MainCategory::onlyTrashed()->findOrFail($id);
+        $mainCategory->subCategories()->withTrashed()->updateExistingPivot($mainCategory->subCategories->pluck('id'), ['deleted_at' => null]);
+        $mainCategory->restore();
+        return true;
     }
-    //========================================================================================================================
 }
