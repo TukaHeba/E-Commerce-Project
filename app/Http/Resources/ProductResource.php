7b78844cf8ca534da09035747ce4ed913160e615
noorhaan-almohammed <gnourhhaan1994@gmail.com>
2024-12-27 21:30:24 +0300

    Add-Featur/Products:model,resource,service,api/add ratings product

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -19,7 +19,8 @@ class ProductResource extends JsonResource
             'name'           => $this->name,
             'description'    => $this->description,
             'price'          => $this->price,
-            'category'       => $this->whenLoaded('category', function() { return $this->category->name;}),
+            'category'       => $this->whenLoaded('category', function() { return $this->category->sub_category_name;}),
+            'average rating' => $this->averageRating() ?? 0,
         ];
     }
 }
