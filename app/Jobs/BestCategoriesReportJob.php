c3875f5cb84aa9788d0702a72dcd8ae6c78bdca5
noorhaan-almohammed <gnourhhaan1994@gmail.com>
2025-01-12 17:26:22 +0300

    Fix-Feature|Job and command best category

--- app/Jobs/BestCategoriesReportJob.php
+++ app/Jobs/BestCategoriesReportJob.php
@@ -4,33 +4,27 @@ namespace App\Jobs;
 
 use App\Models\User\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Support\Facades\Log;
+use App\Services\Export\ExportService;
+use App\Services\Report\ReportService;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Notification;
 use App\Notifications\BestCAtegoriesNotification;
 
 class BestCategoriesReportJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
-    protected $user;
-    protected $file;
-    /**
-     * Create a new job instance.
-     */
-    public function __construct(User $user, $file)
-    {
-        $this->user = $user;
-        $this->file = $file;
-    }
-
     /**
      * Execute the job.
      */
     public function handle(): void
     {
-        $this->user->notify(new BestCategoriesNotification($this->file));
+        $file = (new ExportService(new ReportService()))->bestCategoriesExportStorage();
+        $users = User::role(['sales manager', 'store manager'])->get();
+        foreach ($users as $user) {
+            Notification::send($user, new BestCategoriesNotification($file));
+        }
     }
 }
